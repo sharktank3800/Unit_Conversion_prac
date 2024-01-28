e424917f256182cd912b1758926e89b3f5e61bd4
[Ayman Shiham] <sharktank38000@gmail.com>
2024-01-28 15:43:12 -0500

    complete functions for weight and distance conversions

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ function temperature(){
     var f = (c * 9/5) + 32;
     document.getElementById("fahrenheit").value = f
 }
+
+function weight(){
+    //to convert KGs to Pounds
+    //KG * 2.2
+    var kg = document.getElementById("kilo").value;
+    var p = kg * 2.2
+    document.getElementById("ibs").value = p
+}
+
+function distance(){
+    //to convert Kms to Miles
+    //Kms * 0.62137
+    var kilometers = document.getElementById("km").value;
+    var miles = kilometers * 0.62137
+    document.getElementById("m").value = miles 
+}
